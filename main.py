22b6522057a1c23be34edff3ea68880c23b54bc1
Supanan28284 <supananjame37@gmail.com>
2026-08-25 05:09:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1382,5 +1382,3 @@ async def chat_monitor(request: Request, admin_id: int, customer: Optional[str] 
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-
-```
